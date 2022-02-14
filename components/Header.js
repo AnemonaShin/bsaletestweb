80a35a786c907d12ca041a740861f2553cf2d631
Christian Ramirez <deshkn@hotmail.com>
2022-02-14 00:14:54 -0300

    Vuelta atras

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,3 @@
 export default function Header({ title }) {
-
-  const url = "https://store-bsale-app.herokuapp.com";
-
-  const HTMLResponse = document.querySelector("#app");
-
-  //Creacion de Promesa para leer desde la URL API Categoria.
-  fetch(`${url}/store/list/category`)
-  .then((response) => response.json())
-  .then((categorys) => { 
-      const categoryName = categorys.map(category => `<li>${category.name}</li>`);
-      const categoryId = categorys.map(category => `<li>${category.id}</li>`);
-      return HTMLResponse.innerHTML = `<button name="${categoryName}" type="Submit" value="${categoryId}">${categoryName}</button>`;
-  });
+  return <h1 className="title">{title}</h1>
 }
